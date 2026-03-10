e8864779210313744391f2296dd979bd0f7ecf86
ayalaassaf-dev <ayala.assaf@gmail.com>
2026-03-10 22:36:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import re
 import pandas as pd
 import streamlit as st
+import streamlit.components.v1 as components
 
 # =====================================================
 # ===== הגדרות כלליות של האפליקציה ====================
